2e08ddcd9bf216cae2cb98694aeef2b2221d774a
Kathleen Owen <kathleenowen@MacBook-Air-348.local>
2024-12-03 17:43:04 +0000

    loading data

--- proofofconcept.R
+++ proofofconcept.R
@@ -1,1 +1,7 @@
-#installing lavan package for SEM
+#installing lavaan package for SEM
+
+#install.packages("lavaan")
+library(lavaan)
+
+#loading data 
+proofofcon <- read.csv("~/Desktop/MSc EEB/WD/grantproposal/proofofcon.csv", stringsAsFactors = FALSE, header=TRUE, fileEncoding="latin1")
